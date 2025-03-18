bb10cc2aeaf9e3cb1e8808f5a1219a2588b17275
Manika Sharma <manika.sharma10000@gmail.com>
2025-03-18 13:51:15 +0100

    Name specific coffee

--- AndroidStudioProjects/CoffeeMachine/app/src/main/java/eu/tutorials/coffeemachine/MakeCoffee.kt
+++ AndroidStudioProjects/CoffeeMachine/app/src/main/java/eu/tutorials/coffeemachine/MakeCoffee.kt
@@ -1,17 +1,17 @@
 package eu.tutorials.coffeemachine
 
 fun main(){
-    makeCoffee(2);
+    makeCoffee(2, "Jimmy");
 }
 
-fun makeCoffee(sugarCount: Int) {
+fun makeCoffee(sugarCount: Int, name: String) {
 
     if(sugarCount==0){
-        print("Coffee with no sugar");
+        print("Coffee with no sugar for $name");
     }
     else if(sugarCount==1){
-        println("Coffee with $sugarCount spoon");
+        println("Coffee with $sugarCount spoon for $name");
     }
     else
-        println("Coffee with $sugarCount spoons");
+        println("Coffee with $sugarCount spoons for $name");
 }
